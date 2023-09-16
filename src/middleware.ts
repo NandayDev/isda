2a5aaea5d98f9f62ed621827312011e9faeb24e2
Igor Zanella <igorzanella.dev@gmail.com>
2023-09-16 03:37:27 +0200

    feat: add supabase auth

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    return res;
+  }
+
+  const redirectUrl = req.nextUrl.clone();
+  redirectUrl.pathname = "/";
+  redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
+  return NextResponse.redirect(redirectUrl);
+}
+
+export const config = {
+  matcher: "/private/:path*",
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
+import { Session, SessionContextProvider } from "@supabase/auth-helpers-react";
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { useState } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
+  const [supabaseClient] = useState(() => createPagesBrowserClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
