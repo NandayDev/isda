9f4e432c44f26398c52a4960af88d09d5fd05cc3
Igor Zanella <igorzanella.dev@gmail.com>
2023-09-16 17:17:45 +0200

    feat: add tanstack query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 import { useState } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import "styles/globals.css";
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({
   Component,
@@ -11,16 +13,19 @@ export default function App({
 }: AppProps<{
   initialSession: Session;
 }>) {
+  const [queryClient] = useState(() => new QueryClient());
   const [supabaseClient] = useState(() => createPagesBrowserClient());
 
   return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SessionContextProvider>
+    </QueryClientProvider>
   );
 }
