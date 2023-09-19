48f9e8b9dd6c684c7cb628d5b3cef5b1299f571c
Igor Zanella <igorzanella.dev@gmail.com>
2023-09-19 03:05:36 +0200

    fix: fix GlobalWrapper style

--- src/components/GlobalWrapper/index.tsx
+++ src/components/GlobalWrapper/index.tsx
@@ -1,9 +1,14 @@
 import { FunctionComponent, ReactNode } from "react";
 import Head from "next/head";
-import { Flex, FlexProps, useColorModeValue } from "@chakra-ui/react";
+import {
+  Container,
+  ContainerProps,
+  Flex,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import Header from "components/GlobalWrapper/Header";
 
-interface GlobalWrapperProps extends FlexProps {
+interface GlobalWrapperProps extends ContainerProps {
   children: ReactNode;
   withoutHeader?: boolean;
 }
@@ -18,15 +23,21 @@ const GlobalWrapper: FunctionComponent<GlobalWrapperProps> = ({
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      {!withoutHeader && <Header />}
       <Flex
         as={"main"}
         bg={useColorModeValue("gray.100", "gray.900")}
-        flexDirection={"column"}
         height={"100%"}
-        {...rest}
       >
-        {!withoutHeader && <Header />}
-        {children}
+        <Container
+          display={"flex"}
+          flexDirection={"column"}
+          maxW={"8xl"}
+          py={4}
+          {...rest}
+        >
+          {children}
+        </Container>
       </Flex>
     </>
   );
