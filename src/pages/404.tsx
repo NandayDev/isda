88d86a0b389ddead898688235e204b5618c61e5e
Igor Zanella <igorzanella.dev@gmail.com>
2023-09-29 01:52:34 +0200

    feat: add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,20 @@
 import { FunctionComponent } from "react";
+import GlobalWrapper from "components/GlobalWrapper";
+import { Text } from "@chakra-ui/react";
 
 const Page404: FunctionComponent = () => {
-  return <div>404</div>;
+  return (
+    <GlobalWrapper
+      withoutHeader
+      alignItems={"center"}
+      justifyContent={"center"}
+    >
+      <Text fontSize={"8xl"} fontWeight={"medium"}>
+        404
+      </Text>
+      <Text fontSize={"2xl"}>"Dai, dai dai!"</Text>
+    </GlobalWrapper>
+  );
 };
 
 export default Page404;
