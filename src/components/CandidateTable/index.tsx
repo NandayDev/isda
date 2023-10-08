07edbba7fa427d0e19d295632e190dcec196a882
Igor Zanella <igorzanella.dev@gmail.com>
2023-10-08 18:23:34 +0200

    feat: add delete behavior and fix style of CandidateTable

--- src/components/CandidateTable/index.tsx
+++ src/components/CandidateTable/index.tsx
@@ -1,6 +1,13 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import {
+  Button,
   Flex,
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Table,
   Tbody,
   Td,
@@ -9,140 +16,267 @@ import {
   Thead,
   Tr,
   useColorModeValue,
+  useToast,
 } from "@chakra-ui/react";
 import Image from "next/image";
 import { Voter } from "@prisma/client";
 import { ChatVoteData, VoteCategory, VoteData } from "types/vote";
 import { VOTE_CATEGORY_NAMES } from "constants/vote";
 import { CandidateWithVotesAndCalculations } from "types/candidate";
+import { DeleteIcon } from "@chakra-ui/icons";
+import useCandidateQuery from "network/useCandidateQuery";
 
 interface CandidateTableProps {
   candidate: CandidateWithVotesAndCalculations;
   voters: Voter[];
 }
 
+const FIRST_COLUMN_WIDTH_PERCENTAGE = 25;
+const LAST_COLUMN_WIDTH_PERCENTAGE = 15;
+
 const CandidateTable: FunctionComponent<CandidateTableProps> = ({
   candidate,
   voters,
 }) => {
+  const toast = useToast();
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const columnWidth = `${
+    (100 - FIRST_COLUMN_WIDTH_PERCENTAGE - LAST_COLUMN_WIDTH_PERCENTAGE) /
+    candidate.votes.length
+  }%`;
+
+  const { mutate: deleteCandidate, isLoading: isDeleteCandidateLoading } =
+    useCandidateQuery.delete({
+      onSuccess: () => {
+        handleCloseDeleteModal();
+        toast({
+          title: "Candidato eliminato",
+          description: "Il candidato è stato eliminato correttamente",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+      },
+      onError: (error) => {
+        toast({
+          title: "Errore",
+          description: error.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "top",
+        });
+      },
+    });
+
+  const votersObject = voters.reduce(
+    (acc, voter) => {
+      acc[voter.id] = voter;
+      return acc;
+    },
+    {} as Record<string, Voter>
+  );
+
+  const handleOpenDeleteModal = () => {
+    setShowDeleteModal(true);
+  };
+
+  const handleCloseDeleteModal = () => {
+    setShowDeleteModal(false);
+  };
+
+  const handleDeleteCandidate = () => {
+    deleteCandidate({ candidateId: candidate.id });
+  };
+
   return (
-    <Table bg={useColorModeValue("white", "black")}>
-      <Thead>
-        <Tr>
-          <Th fontSize={"lg"}>
-            {candidate.name}: {candidate.calculations.totalAverage}
-          </Th>
-          {voters.map((voter) => (
-            <Th key={voter.id}>
-              <Flex alignItems={"center"} direction={"column"} gap={2}>
-                {voter.image && (
-                  <Image
-                    src={voter.image}
-                    alt={voter.name}
-                    width={50}
-                    height={50}
-                  />
-                )}
-                {voter.name}
+    <>
+      <Table bg={useColorModeValue("white", "black")}>
+        <Thead>
+          <Tr>
+            <Th fontSize={"lg"} w={`${FIRST_COLUMN_WIDTH_PERCENTAGE}%`}>
+              <Flex alignItems={"center"} gap={2}>
+                <Text lineHeight={5}>
+                  {candidate.name}: {candidate.calculations.totalAverage}
+                </Text>
+                <DeleteIcon
+                  fontSize={"sm"}
+                  color={"red"}
+                  cursor={"pointer"}
+                  onClick={handleOpenDeleteModal}
+                />
               </Flex>
             </Th>
-          ))}
-          <Th>
-            <Text align={"center"}>Media</Text>
-          </Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {Object.values(VoteCategory).map((category) => (
-          <Tr key={category}>
-            <Td>
-              <Text fontWeight={"medium"}>{VOTE_CATEGORY_NAMES[category]}</Text>
-            </Td>
-            {voters.map((voter) => (
-              <Td key={voter.id}>
-                <Text align={"center"}>
-                  {
-                    (
-                      candidate.votes.find((vote) => vote.voterId === voter.id)
-                        ?.voteData as VoteData
-                    )[category]
-                  }
+            {candidate.votes.map(({ voterId }) => {
+              const voter = votersObject[voterId];
+
+              return (
+                <Th key={voter.id} w={columnWidth}>
+                  <Flex alignItems={"center"} direction={"column"} gap={2}>
+                    {voter.image && (
+                      <Image
+                        src={voter.image}
+                        alt={voter.name}
+                        width={50}
+                        height={50}
+                      />
+                    )}
+                    {votersObject[voterId].name}
+                  </Flex>
+                </Th>
+              );
+            })}
+            <Th w={`${LAST_COLUMN_WIDTH_PERCENTAGE}%`}>
+              <Text align={"center"}>Media</Text>
+            </Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {Object.values(VoteCategory).map((category) => (
+            <Tr key={category}>
+              <Td>
+                <Text fontWeight={"medium"}>
+                  {VOTE_CATEGORY_NAMES[category]}
                 </Text>
               </Td>
-            ))}
+              {candidate.votes.map(({ voterId }) => {
+                const voter = votersObject[voterId];
+
+                return (
+                  <Td key={voter.id}>
+                    <Text align={"center"}>
+                      {
+                        (
+                          candidate.votes.find(
+                            (vote) => vote.voterId === voter.id
+                          )?.voteData as VoteData
+                        )[category]
+                      }
+                    </Text>
+                  </Td>
+                );
+              })}
+              <Td>
+                <Text align={"center"}>
+                  {candidate.calculations.averagesByCategory[category]}
+                </Text>
+              </Td>
+            </Tr>
+          ))}
+          <Tr>
+            <Td>
+              <Text fontWeight={"medium"}>Voto giudici</Text>
+            </Td>
+            {candidate.votes.map(({ voterId }) => {
+              const voter = votersObject[voterId];
+              return (
+                <Td key={voter.id}>
+                  <Text align={"center"}>
+                    {candidate.calculations.totalsByVoter[voter.id]}
+                  </Text>
+                </Td>
+              );
+            })}
             <Td>
               <Text align={"center"}>
-                {candidate.calculations.averagesByCategory[category]}
+                {candidate.calculations.votersAverage}
               </Text>
             </Td>
           </Tr>
-        ))}
-        <Tr>
-          <Td>
-            <Text fontWeight={"medium"}>Voto giudici</Text>
-          </Td>
-          {voters.map((voter) => (
-            <Td key={voter.id}>
-              <Text align={"center"}>
-                {candidate.calculations.totalsByVoter[voter.id]}
+          {candidate.votes.some((vote) => vote.chatVoteData) && (
+            <Tr>
+              <Td>
+                <Text fontWeight={"medium"}>Voto chat</Text>
+              </Td>
+              {candidate.votes.map(({ voterId }) => {
+                const voter = votersObject[voterId];
+                return (
+                  <Td key={voter.id}>
+                    {candidate.votes.find((vote) => vote.voterId === voter.id)
+                      ?.chatVoteData && (
+                      <Text align={"center"}>
+                        {
+                          (
+                            candidate.votes.find(
+                              (vote) => vote.voterId === voter.id
+                            )?.chatVoteData as ChatVoteData
+                          )?.positivePercentage
+                        }
+                        % 👍 su{" "}
+                        {
+                          (
+                            candidate.votes.find(
+                              (vote) => vote.voterId === voter.id
+                            )?.chatVoteData as ChatVoteData
+                          )?.voters
+                        }{" "}
+                        voti
+                      </Text>
+                    )}
+                  </Td>
+                );
+              })}
+              <Td>
+                <Text align={"center"}>
+                  {candidate.calculations.chatsAverage}
+                </Text>
+              </Td>
+            </Tr>
+          )}
+          <Tr>
+            <Td>
+              <Flex alignItems={"center"} gap={2}>
+                <Text fontSize={"sm"}>
+                  Inserito il{" "}
+                  {new Date(candidate.createdAt).toLocaleDateString("it", {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                  })}{" "}
+                  alle ore{" "}
+                  {new Date(candidate.createdAt).toLocaleTimeString("it")}
+                </Text>
+              </Flex>
+            </Td>
+            <Td colSpan={candidate.votes.length}>
+              <Text align={"right"} fontWeight={"medium"} fontSize={"2xl"}>
+                Media totale
               </Text>
             </Td>
-          ))}
-          <Td>
-            <Text align={"center"}>{candidate.calculations.votersAverage}</Text>
-          </Td>
-        </Tr>
-        <Tr>
-          <Td>
-            <Text fontWeight={"medium"}>Voto chat</Text>
-          </Td>
-          {voters.map((voter) => (
-            <Td key={voter.id}>
-              {candidate.votes.find((vote) => vote.voterId === voter.id)
-                ?.chatVoteData && (
-                <Text align={"center"}>
-                  {
-                    (
-                      candidate.votes.find((vote) => vote.voterId === voter.id)
-                        ?.chatVoteData as ChatVoteData
-                    )?.positivePercentage
-                  }
-                  % 👍 su{" "}
-                  {
-                    (
-                      candidate.votes.find((vote) => vote.voterId === voter.id)
-                        ?.chatVoteData as ChatVoteData
-                    )?.voters
-                  }{" "}
-                  voti
-                </Text>
-              )}
+            <Td>
+              <Text align={"center"} fontWeight={"medium"} fontSize={"2xl"}>
+                {candidate.calculations.totalAverage}
+              </Text>
             </Td>
-          ))}
-          <Td>
-            <Text align={"center"}>{candidate.calculations.chatsAverage}</Text>
-          </Td>
-        </Tr>
-        <Tr>
-          <Td>
-            <Text fontSize={"sm"}>
-              Inserito il {new Date(candidate.createdAt).toLocaleDateString()}{" "}
-              alle ore {new Date(candidate.createdAt).toLocaleTimeString()}
-            </Text>{" "}
-          </Td>
-          <Td colSpan={voters.length}>
-            <Text align={"right"} fontWeight={"medium"} fontSize={"2xl"}>
-              Media totale
-            </Text>
-          </Td>
-          <Td>
-            <Text align={"center"} fontWeight={"medium"} fontSize={"2xl"}>
-              {candidate.calculations.totalAverage}
-            </Text>
-          </Td>
-        </Tr>
-      </Tbody>
-    </Table>
+          </Tr>
+        </Tbody>
+      </Table>
+      <Modal
+        isCentered
+        isOpen={showDeleteModal}
+        onClose={handleCloseDeleteModal}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Sei sicuro di eliminare {candidate.name}?</ModalHeader>
+          <ModalCloseButton />
+          <ModalFooter gap={2}>
+            <Button variant={"ghost"} onClick={handleCloseDeleteModal}>
+              Chiudi
+            </Button>
+            <Button
+              colorScheme={"red"}
+              isLoading={isDeleteCandidateLoading}
+              onClick={handleDeleteCandidate}
+            >
+              Elimina
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
